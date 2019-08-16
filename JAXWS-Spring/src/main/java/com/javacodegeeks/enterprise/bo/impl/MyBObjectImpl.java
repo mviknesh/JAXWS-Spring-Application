d5cf87a1d4318cbab0f381ff072338219e21e743
mviknesh <51973552+mviknesh@users.noreply.github.com>
2019-08-16 15:03:49 -0500

    initial checkin
    initial checkin

---
+++ JAXWS-Spring/src/main/java/com/javacodegeeks/enterprise/bo/impl/MyBObjectImpl.java
@@ -0,0 +1,15 @@
+package com.javacodegeeks.enterprise.bo.impl;
+
+import com.javacodegeeks.enterprise.bo.MyBObject;
+
+public class MyBObjectImpl implements MyBObject {
+
+	public String printMessage() {
+		// TODO Auto-generated method stub
+		 return "Hello from Java Code Geeks JAXWS with Spring Tutorial";
+	}
+ 
+    /*public String printMessage() {
+        return "Hello from Java Code Geeks JAXWS with Spring Tutorial";
+    }*/
+}
